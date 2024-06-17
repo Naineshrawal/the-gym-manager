ed81a22ae9fa05c81880b40ee5f659e76e5cd396
nainesh raval <naineshrawal@gmail.com>
2024-06-17 13:41:26 +0530

    initialize project

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        brand: {
+          primary: '#ef476f',
+          secondary: '#ffd166',
+          accent: '#06d6a0',
+          neutral: '#118ab2',
+          dark: '#073b4c',
+        }
+      }
+    },
+  },
+  plugins: [],
+}
+
